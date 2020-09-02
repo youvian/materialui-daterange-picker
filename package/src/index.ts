1dd84cd5966db6393770cb3d54be006a20589c48
Mathias Lykkegaard Lorenzen <mathias.lorenzen@live.com>
2020-09-02 13:10:25 +0200

    Update index.ts

--- package/src/index.ts
+++ package/src/index.ts
@@ -1,8 +1,10 @@
 import DateRangePickerExporter from './components/DateRangePickerExporter';
+import DateRangePicker from './components/DateRangePicker';
 import { DateRange, DefinedRange } from './types';
 
 export {
-  DateRangePickerExporter as DateRangePicker,
+  DateRangePickerExporter as DateRangePickerOverlay,
+  DateRangePicker as DateRangePicker,
   DefinedRange,
   DateRange,
 };
